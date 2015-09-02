1354c4e536b71911a07df8bbace015a47e93717c
dhanutpk <dhanu.tpk@gmail.com>
2015-09-02 15:40:47 +0530

    MVC Crud Operation
    MVC Crud Operation

---
+++ WebApp1/WebApp1/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApp1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+
+    sealed internal class ProductContext : DbContext
+    {
+        public ProductContext()
+            : base("ProductConnection")
+        {
+
+        }
+        
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasKey(p => p.ProductId);
+
+            modelBuilder.Entity<Product>().Property(p => p.ProductId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(200).HasColumnType("nvarchar");
+            modelBuilder.Entity<Product>().Property(p => p.Description).IsOptional().HasMaxLength(200).HasColumnType("nvarchar");
+            modelBuilder.Entity<Product>().Property(p => p.CreatedDate).IsOptional();
+            
+            base.OnModelCreating(modelBuilder);
+        }    
+    }
+}
